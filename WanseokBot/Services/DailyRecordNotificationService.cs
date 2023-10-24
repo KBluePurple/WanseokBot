d84103dec6e35e48521eef8d573dc4a9842ab1af
KBluePurple <42506968+blwon@users.noreply.github.com>
2023-10-24 22:36:03 +0900

    파일 분리

--- WanseokBot/Services/DailyRecordNotificationService.cs
+++ WanseokBot/Services/DailyRecordNotificationService.cs
@@ -1,4 +1,3 @@
-using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -63,41 +62,3 @@ public class DailyRecordNotificationService : IHostedService
 }
 
 // ReSharper disable once ClassNeverInstantiated.Global
-public class DailyRecordNotificationJob : IJob
-{
-    public async Task Execute(IJobExecutionContext context)
-    {
-        Console.WriteLine("DailyRecordNotificationJob executed.");
-
-        var dataMap = context.JobDetail.JobDataMap;
-
-        var calendarService = (CalenderService)dataMap["calenderService"];
-
-        if (calendarService.IsHoliday(DateTime.Now)) return;
-
-        var client = (DiscordSocketClient)dataMap["client"];
-        var guildId = (ulong)dataMap["guildId"];
-        var channelId = (ulong)dataMap["channelId"];
-        var roleId = (ulong)dataMap["roleId"];
-
-        var guild = client.GetGuild(guildId);
-        var channel = guild.GetTextChannel(channelId);
-        var role = guild.GetRole(roleId);
-
-        var embed = new EmbedBuilder()
-            .WithColor(role.Color)
-            .WithAuthor("실습 일지 알림!", guild.IconUrl)
-            .WithDescription(
-                $"""
-                 <@&{roleId}>이신 분들은 모두 오늘의 실습 일지를 작성해주세요!
-                 [실습 일지 작성 바로가기](https://www.hifive.go.kr/mobile/mInvovedReportingMain.do)
-                 """
-            )
-            .WithTimestamp(DateTimeOffset.Now)
-            .Build();
-
-        var text = $"||<@&{roleId}>||";
-
-        await channel.SendMessageAsync(text, embed: embed);
-    }
-}
